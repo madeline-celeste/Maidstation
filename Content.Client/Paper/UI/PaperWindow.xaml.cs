space-wizards/space-station-14

Fix news console formatting and pda news formating (#41799)
* Fix news console formatting

* another fix

* Fix review

Un-copy-paste. Twice. *sigh*

---------

Co-authored-by: PJB3005 <pieterjan.briers+git@gmail.com>

Content.Client/Paper/UI/PaperWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using Content.Client.RichText;
 using Content.Shared.Paper;
 using Robust.Client.AutoGenerated;
 using Robust.Client.Graphics;
@@ -37,16 +38,6 @@ namespace Content.Client.Paper.UI
         // If paper limits the size in one or both axes, it'll affect whether
         // we're able to resize this UI or not. Default to everything enabled:
         private DragMode _allowedResizeModes = ~DragMode.None;
-
-        private readonly Type[] _allowedTags = new Type[] {
-            typeof(BoldItalicTag),
-            typeof(BoldTag),
-            typeof(BulletTag),
-            typeof(ColorTag),
-            typeof(HeadingTag),
-            typeof(ItalicTag),
-            typeof(MonoTag)
-        };
 
         public event Action<string>? OnSaved;
 
@@ -280,7 +271,7 @@ namespace Content.Client.Paper.UI
             {
                 msg.AddMarkupPermissive("\r\n");
             }
-            WrittenTextLabel.SetMessage(msg, _allowedTags, DefaultTextColor);
+            WrittenTextLabel.SetMessage(msg, UserFormattableTags.BaseAllowedTags, DefaultTextColor);
 
             WrittenTextLabel.Visible = !isEditing && state.Text.Length > 0;
             BlankPaperIndicator.Visible = !isEditing && state.Text.Length == 0;

madeline-celeste/Maidstation

Content.Client/Paper/UI/PaperWindow.xaml.cs
@@ -84,6 +84,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using System.Numerics;
+using Content.Client.RichText;
 using Content.Shared.Paper;
 using Robust.Client.UserInterface; // Starlight-edit
 using Robust.Client.AutoGenerated;
@@ -129,20 +130,7 @@ namespace Content.Client.Paper.UI
         // Store original margin to restore when switching modes // Starlight-edit
         private Thickness _originalContentMargin; // Starlight-edit
 
-        private readonly Type[] _allowedTags = new Type[] {
-            typeof(BoldItalicTag),
-            typeof(BoldTag),
-            typeof(BulletTag),
-            typeof(ColorTag),
-            typeof(HeadingTag),
-            typeof(ItalicTag),
-            typeof(MonoTag),
-            // Starlight paperwork edit
-            typeof(FormTagHandler), //
-            typeof(SignatureTagHandler),
-            typeof(CheckTagHandler)
-            // Starlight paperwork end
-        };
+        // goobstation upstreamer note if you're looking for _allowedtags its moved to UserFormattableTags.cs
 
         public event Action<string>? OnSaved;
         public event Action<int>? OnSignatureRequested; // Starlight-edit
@@ -400,7 +388,7 @@ namespace Content.Client.Paper.UI
 
             var fm = new FormattedMessage();
             fm.AddMarkupPermissive(state.Text);
-            WrittenTextLabel.SetMessage(fm, _allowedTags, DefaultTextColor);
+            WrittenTextLabel.SetMessage(fm, UserFormattableTags.BaseAllowedTags, DefaultTextColor);
 
             // Add extra bottom margin based on tag count to prevent cutoff (only in read mode)
             var tagCount = CountTags(state.Text);